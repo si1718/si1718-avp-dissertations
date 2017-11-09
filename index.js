8e861a3b86ca6b571c6abf0c8c96d931a4a9f85b
Alvaro Valencia <avalenciaparra@gmail.com>
2017-11-09 17:30:04 +0000

    feat: base api created

--- index.js
+++ index.js
@@ -1,7 +1,104 @@
-var express = require("express")
+var express = require('express');
+var bodyParser = require("body-parser");
+var helmet = require("helmet");
+var path = require('path');
+var MongoClient = require("mongodb").MongoClient;
+var utils = require("./utils.js");
 
 var app = express();
 
-app.use(express.static('./'))
+app.use(bodyParser.json()); //configura dentro de express el middleware bodyparser json
 
-app.listen(process.env.PORT);
+var port = (process.env.PORT || 10000);
+
+// Database initialization
+var db;
+var mdbURL = "mongodb://user:user@ds255455.mlab.com:55455/si1718-avp-dissertations";
+
+MongoClient.connect(mdbURL, { native_parser: true }, function(err, database) {
+    if (err) {
+        console.log("CAN NOT CONNECT TO DB: " + err);
+        process.exit(1);
+    }
+    
+    db = database.collection("dissertations");
+
+    app.listen(port, () => {
+        console.log("Magic is happening on port " + port);
+    });
+});
+
+
+// API stuffs
+var BASE_API_PATH = "/api/v1";
+
+// RETRIEVE all dissertations
+app.get(BASE_API_PATH + '/dissertations', function(req, res) {
+    db.find({}).toArray((err, elements) => {
+        if (err)
+            res.send(err);
+        else
+            res.send(elements);
+    });
+});
+
+// RETRIEVE a specific dissertation
+app.get(BASE_API_PATH + '/dissertations/:id', function(req, res) {
+    var id = req.params.id;
+
+    db.findOne({ "id": id }, (err, elements) => {
+        if (err)
+            res.send(err);
+        else
+            res.send(elements);
+    });
+});
+
+// CREATE a dissertation
+app.post(BASE_API_PATH + '/dissertations', function(req, res) {
+    var id = utils.generateDissertationId(req.body.author, req.body.year);
+    req.body.id = id;
+
+    db.insertOne(req.body, (err, result) => {
+        if (err)
+            res.send(err)
+        else {
+            res.sendStatus(201);
+        }
+    });
+});
+
+// UPDATE a specific dissertation
+app.put(BASE_API_PATH + '/dissertations/:id', function(req, res) {
+    var id = req.params.id;
+
+    db.updateOne({ "id": id }, { $set: req.body }, (err, result) => {
+        if (err)
+            res.send(err);
+        else
+            res.sendStatus(200);
+    });
+});
+
+// DELETE all dissertations
+app.delete(BASE_API_PATH + '/dissertations', function(req, res) {
+    db.deleteMany({}, (err, result) => {
+        if (err)
+            res.send(err)
+        else {
+            res.sendStatus(200);
+        }
+    });
+});
+
+// DELETE a specific dissertation
+app.delete(BASE_API_PATH + '/dissertations/:id', function(req, res) {
+    var id = req.params.id;
+
+    db.deleteOne({ "id": id }, (err, result) => {
+        if (err)
+            res.send(err);
+        else
+            res.sendStatus(200);
+    });
+});
---
+++ utils.js
@@ -0,0 +1,26 @@
+var exports = module.exports = {};
+
+// obtained from http://www.etnassoft.com/2011/03/03/eliminar-tildes-con-javascript/
+// gets a String with punctuation signs and returns a String without them
+var normalize = (function() {
+    var from = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç",
+        to = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc",
+        mapping = {};
+    for (var i = 0, j = from.length; i < j; i++)
+        mapping[from.charAt(i)] = to.charAt(i);
+    return function(str) {
+        var ret = [];
+        for (var i = 0, j = str.length; i < j; i++) {
+            var c = str.charAt(i);
+            if (mapping.hasOwnProperty(str.charAt(i)))
+                ret.push(mapping[c]);
+            else
+                ret.push(c);
+        }
+        return ret.join('');
+    }
+})();
+
+exports.generateDissertationId = function(author, year) {
+    return normalize(author).toLowerCase().replace(/,/g, "").replace(/ /g, "-") + "-" + year.toString();
+}
