1210bc223ef31b83016f42933b5a9e15875f51ca
Alvaro Valencia <avalenciaparra@gmail.com>
2017-11-21 18:21:43 +0000

    added frontend static route in app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 var express = require("express");
 var app = express();
 var db = require("./db");
-
 var argv = require('minimist')(process.argv.slice(2));
 var swagger = require("swagger-node-express");
 var bodyParser = require( 'body-parser' );
-
+var path = require("path");
 
 
 var BASE_API_PATH = "/api/v1";
 
+// API controllers
 var DissertationController = require("./dissertation/DissertationController");
 app.use(BASE_API_PATH + "/dissertations", DissertationController);
 
+// frontend
+app.use(express.static(path.join(__dirname, "public")));
 
-//var subpath = express();
+// swagger
 app.use(bodyParser());
-//app.use("/docs", subpath);
-//swagger.setAppHandler(subpath);
 swagger.setAppHandler(app);
 
 app.use("/docs/v1", express.static('docs/dist'));
@@ -29,12 +29,7 @@ swagger.setApiInfo({
     license: "",
     licenseUrl: ""
 });
-/*
-subpath.get('/docs/v1', function (req, res) {
-    res.sendfile(__dirname + '/docs/dist/index.html');
-});
-swagger.configureSwaggerPaths('', 'api-docs', '');
-*/
+
 var domain = 'localhost';
 if(argv.domain !== undefined)
     domain = argv.domain;
@@ -42,9 +37,5 @@ else
     console.log('No --domain=xxx specified, taking default hostname "localhost".');
 var applicationUrl = 'http://' + domain;
 swagger.configure(applicationUrl, '1.0.0');
-
-
-
-
 
 module.exports = app;
