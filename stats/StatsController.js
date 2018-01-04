8fd8a0f687b74f3bb463ccf770fc2afc05147620
Alvaro Valencia <avalenciaparra@gmail.com>
2018-01-04 15:59:12 +0000

    added twitter stats to backend

--- stats/StatsController.js
+++ stats/StatsController.js
@@ -10,6 +10,7 @@ router.use(helmet()); //improve security
 var DissertationsPerYear = require("./DissertationsPerYear");
 var DissertationsPerTutor = require("./DissertationsPerTutor");
 var MostFrequentKeywords = require("./MostFrequentKeywords");
+var TwitterKeywords = require("./TwitterKeywords");
 
 // RETRIEVE all dissertationsPerYear stats
 router.get('/dissertationsPerYear', function(req, res) {
@@ -48,6 +49,20 @@ router.get('/mostFrequentKeywords', function(req, res) {
         }
         else {
             console.log("INFO: New GET request to /mostFrequentKeywords");
+            res.send(elements);
+        }
+    });
+});
+
+// RETRIEVE all twitterKeywords stats
+router.get('/twitterKeywords', function(req, res) {
+    TwitterKeywords.find({}, (err, elements) => {
+        if (err) {
+            console.error('WARNING: Error getting data from DB => ' + err);
+            res.sendStatus(500); // internal server error
+        }
+        else {
+            console.log("INFO: New GET request to /twitterKeywords");
             res.send(elements);
         }
     });
---
+++ stats/TwitterKeywords.js
@@ -0,0 +1,14 @@
+// for further help about mongoose schemas, please refer to http://mongoosejs.com/docs/schematypes.html
+var mongoose = require("mongoose");
+
+var TwitterKeywordsSchema = new mongoose.Schema({
+    keyword: { type: String, required: true },
+    count: { type: Number, required: true },
+    date: { type: Date, required: true },
+}, { collection: "stats" });
+
+TwitterKeywordsSchema.index({ '$**': 'text' });
+
+mongoose.model("TwitterKeywords", TwitterKeywordsSchema);
+
+module.exports = mongoose.model("TwitterKeywords");
